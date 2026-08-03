0b76985fb15df22106833f5688ef247513d19032
Tejaswini Konulada <konuladaanjireddy@gmail.com>
2026-08-03 15:03:13 +0530

    Added project screenshots

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask,render_template,request,jsonify
 import ollama
-import os
-from dotenv import load_dotenv
 app = Flask(__name__)
-load_dotenv()
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 @app.route("/")
 def home():
     return render_template("index.html")
